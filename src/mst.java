d74bae25a21ae4a62c12f82e1559513368bba4b2
Wenxi Wu <Wenxi.wu@anu.edu.au>
2018-10-24 19:01:38 +1100

    new mst

--- src/mst.java
+++ src/mst.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
 public class mst {
+
+    Graph graphs[];
+
+    mst(int number, int graphSize){
+        graphs = new Graph[number];
+        for(int i = 0; i < number; i++){
+            graphs[i] = new Graph(graphSize);
+        }
+//        for(Graph g : graphs){
+//            g = new Graph(graphSize);
+//        }
+    }
 
     public class Graph{
         int V; // number of nodes
@@ -11,6 +24,7 @@ public class mst {
 
 
         Graph(int V){
+            System.out.println("graph size is = "+ V);
             this.V = V;
             adj = new double[V][V];
             nodes = new Node[V];
@@ -34,7 +48,7 @@ public class mst {
             Edge(int from, int to){
                 from = from;
                 to = to;
-                double dx = nodes[from].x - nodes[to].x;
+                double dx = nodes[from].x - nodes[to].x;System.out.println();
                 double dy = nodes[from].y - nodes[to].y;
                 weight = Math.sqrt(dx*dx + dy*dy);
             }
@@ -58,6 +72,39 @@ public class mst {
                 x = min +  (max - min) * r.nextDouble();
                 y = min +  (max - min) * r.nextDouble();
             }
+        }
+
+        public double KruskalMST(){
+            Edge results[] = new Graph.Edge[V];
+            Edge edges[] = new Graph.Edge[(V-1)*(V)/2];
+            Subset subsets[] = new Subset[V];
+            int index = 0;
+            for(int i = 0; i < V; i++){
+                subsets[i] = new Subset(i);
+                for(int j=0; j < V; j++){
+                    edges[index] = new Edge(i, j);
+                    index++;
+                }
+            }
+
+            Arrays.sort(edges);
+
+            int e=0;
+            int i = 0;
+            while(e < V -1){
+                Edge next = edges[i++];
+                int x = find_set(subsets, next.from);
+                int y = find_set(subsets, next.to);
+                if(x != y){
+                    results[e++] = next;
+                    union_set(subsets, x, y);
+                }
+            }
+            int sum = 0;
+            for(Edge edge : results){
+                sum += edge.weight;
+            }
+            return sum;
         }
     }
 
@@ -91,13 +138,19 @@ public class mst {
         }
     }
 
-    public void KruskalMST(Graph g){
-        Graph.Edge results[] = new Graph.Edge[g.V];
-        Subset subsets[] = new Subset[g.V];
-        for(Graph.Node n : g.nodes){
 
+    public static void main(String[] args) {
+        Random r = new Random();
+        double sum_size_100 = 0;
+        int p_size_100 = r.nextInt(50)+50;
+        System.out.println(p_size_100);
+        mst test_size_100 = new mst(p_size_100, 2);
+        System.out.println("size=" + test_size_100.graphs[0].V);
+        for(Graph g: test_size_100.graphs){
+            sum_size_100 += g.KruskalMST();
         }
-    }
+        System.out.println(sum_size_100/p_size_100);
 
+    }
 
 }
